6900ff5b879867c02a44c2b9223cab8c7fefd58c
Mehd0ch <mehdicybsec@gmail.com>
2025-06-05 21:13:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,20 +5,25 @@ if [ $? -ne 0 ]; then
   docker compose up registry -d
   sleep 3
 fi
-echo "------ DEBUT DU BUILD------
+
+echo "------ DEBUT DU BUILD ------"
 docker build -t backend ./backend
-echo "------FIN DE BUILD
-echo "-------DEBUT DU RUN--------
+echo "------ FIN DU BUILD ------"
+
+echo "------- DEBUT DU RUN --------"
 docker compose up backend -d
-echo "------FIN DU RUN ---------
+echo "------ FIN DU RUN ---------"
+
 sleep 15
-echo "-------TEST DU REVERSE PROXY
+
+echo "------- TEST DU REVERSE PROXY -------"
 curl -X POST http://localhost:8081/items \
   -H "Content-Type: application/json" \
   -d '{"name": "name", "value": "value"}'
 
 docker tag backend localhost:50000/backend
-echo "----------STOCKAGE DE L'IMAGE-----"
-docker push localhost:50000/backend
-echo "FIN DU SCRIPT"
 
+echo "---------- STOCKAGE DE L'IMAGE ----------"
+docker push localhost:50000/backend
+
+echo "FIN DU SCRIPT"
