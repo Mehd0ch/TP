7edb58671d82474143daaca1cc44feaf5b00a9fd
Mehd0ch <mehdicybsec@gmail.com>
2025-06-05 21:59:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "------ DEBUT DU BUILD------
+echo "------ DEBUT DU BUILD------"
 docker build -t backend ./backend
-echo "------FIN DE BUILD
-echo "-------DEBUT DU RUN--------
+echo "------FIN DE BUILD"
+echo "-------DEBUT DU RUN--------"
 docker compose up backend -d
-echo "------FIN DU RUN ---------
+echo "------FIN DU RUN ---------"
 sleep 15
-echo "-------TEST DU REVERSE PROXY
+echo "-------TEST DU REVERSE PROXY"
 curl -X POST http://localhost:8081/items \
   -H "Content-Type: application/json" \
   -d '{"name": "name", "value": "value"}'
