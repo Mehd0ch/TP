f8280aafec2cc358fe0106bd66a659b9bed74f94
Mehd0ch <mehdicybsec@gmail.com>
2025-06-05 21:56:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-curl -fs localhost:50000/v2/_catalog > /dev/null
-if [ $? -ne 0 ]; then
-  docker compose -f /home/med/tp/docker-compose.yml up registry -d
-  sleep 3
-fi
-
-echo "------ DEBUT DU BUILD ------"
-docker build -t backend /home/med/tp/backend
-echo "------ FIN DU BUILD ------"
-
-echo "------- DEBUT DU RUN --------"
-docker compose -f /home/med/tp/docker-compose.yaml up backend -d
-echo "------ FIN DU RUN ---------"
-
+echo "------ DEBUT DU BUILD------
+docker build -t backend ./backend
+echo "------FIN DE BUILD
+echo "-------DEBUT DU RUN--------
+docker compose up backend -d
+echo "------FIN DU RUN ---------
 sleep 15
-
-echo "------- TEST DU REVERSE PROXY -------"
+echo "-------TEST DU REVERSE PROXY
 curl -X POST http://localhost:8081/items \
   -H "Content-Type: application/json" \
   -d '{"name": "name", "value": "value"}'
 
 docker tag backend localhost:50000/backend
-
-echo "---------- STOCKAGE DE L'IMAGE ----------"
+echo "----------STOCKAGE DE L'IMAGE-----"
 docker push localhost:50000/backend
-
 echo "FIN DU SCRIPT"
